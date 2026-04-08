c4623dc3e0a826290ee388ebc9cc508726b8f44a
Nicholas Young <qdb21131@archie-w.hpc.strath.ac.uk>
2026-04-08 20:10:14 +0100

    Setting up scripts for task 1 and 2. For my current understanding I am think the easiest way for me to about this is breaking up scripts into core functions like we see here. This is also good as we will be using metropolis for both the ising and XY model that needs to be coded later so this is somewhat efficient in that sense.
    I am about to begin task1 by bmaking an Ising model class which has all the key
    properties of the model.

---
+++ isingmod.py
@@ -0,0 +1,95 @@
+#!/opt/software/anaconda/python-3.10.9/bin/python
+
+# =============================================================================
+# MIT License
+#
+# Copyright (c) 2026 Nicholas Young
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# =============================================================================
+
+"""
+Version: Python 3.10.9
+
+2D nearest-neighbour Ising model on an L x L square lattice.
+
+Hamiltonian is:
+    H = -J * sum_{<i,j>} s_i * s_j
+
+where s_i in {-1, +1} and the sum runs over nearest-neighbour pairs.
+Periodic boundary conditions are applied, where monte-carlo randdom
+walkers are implemented to evaluate lattice points.
+
+Date: 15/04/2026
+
+Author: Nicholas Young
+"""
+
+import numpy as np
+
+"""
+Ideas (delete later):
+
+Create Ising model as a class of functions which describe the core principles
+of the model.
+
+To achieve that we will need to realise the following functions in code:
+
+Lattice creation and random spin application.
+
+Hamiltonian calculations (local so global calculation can be found)
+
+magnetisation calculations (same as hamiltonian)
+
+Energies are bound through spin (+1,-1) coupling between lattice sites
+where two of the same spin are energetically weaker than two of
+opposing spins. This is from Pauli exclusion.
+
+For ferromagnetism J = 1 (J is coupling strength)
+
+These mag and E are needed to find the critical temp from what I can tell.
+
+Thats where ferromagnetism breaks down causing paramagnetism.
+
+From reading up We need to make sure these energy and magnetisation values 
+can be updated regularly with the lattice changes that metropolis will make,
+such as flipping lattice spins.
+
+"""
+
+class IsingModel:
+
+    """2D Ising model on a square lattice.
+
+    Attributes:
+        size: Linear dimension L of the L x L lattice.
+        coupling: Exchange coupling constant J.
+        n_sites: Total number of lattice sites (L * L).
+        spins: Array of spin values, each in {-1, +1}.
+    """
+
+
+
+
+
+
+
+
+
+
